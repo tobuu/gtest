b7e22d8ee4873b56f02131eefaf34330f6f4a0fd
Ainis <Ainis@Ainis-PC>
2013-05-29 07:29:43 +0300

    initial commit

---
+++ src/gtest/Gtest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gtest;
+
+/**
+ *
+ * @author Ainis
+ */
+public class Gtest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //aaaa
+    }
+}

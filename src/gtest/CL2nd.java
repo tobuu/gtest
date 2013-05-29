d80926c0f8b4c0cd008f2ba17d72b2dd052b1314
Ainis <Ainis@Ainis-PC>
2013-05-29 07:52:26 +0300

    BR3 C4 new class

---
+++ src/gtest/CL2nd.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gtest;
+
+/**
+ *
+ * @author Ainis
+ */
+public class CL2nd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void donothing() {
+        //DN 1
+        
+    }
+}
